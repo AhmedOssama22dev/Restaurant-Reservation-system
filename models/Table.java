49215cde2c7832e1b7bc51b6b71e93a30b5294fb
Ahmed Ossama <ahmedossama640@gmail.com>
2020-04-01 11:15:38 +0000

    Update Table.java isReserved,getters and setters added

--- models/Table.java
+++ models/Table.java
@@ -4,16 +4,56 @@ public class Table {
 	public Integer tableNumber;
 	public Integer seatsCount;
 	public Boolean isSmoking;	
+	public Boolean isReserved;
 
-	public Table(Integer tableNumber, Integer seatsCount, Boolean isSmoking) {
-		super();
+	public Integer getTableNumber() {
+		return tableNumber;
+	}
+
+	public void setTableNumber(Integer tableNumber) {
 		this.tableNumber = tableNumber;
+	}
+
+	public Integer getSeatsCount() {
+		return seatsCount;
+	}
+
+	public void setSeatsCount(Integer seatsCount) {
 		this.seatsCount = seatsCount;
+	}
+
+	public Boolean getIsSmoking() {
+		return isSmoking;
+	}
+
+	public void setIsSmoking(Boolean isSmoking) {
 		this.isSmoking = isSmoking;
+	}
+
+	public Boolean getIsReserved() {
+		return isReserved;
+	}
+
+	public void setIsReserved(Boolean isReserved) {
+		this.isReserved = isReserved;
+	}
+
+	public Table(Integer tableNumber, Integer seatsCount, Boolean isSmoking,Boolean isReserved) {
+		super();
+		setTableNumber(tableNumber);
+		setSeatsCount(seatsCount);
+		setIsSmoking(isSmoking);
+		setIsReserved(isReserved);
+		
 	}
 	
 	@Override
 	public String toString() {
-		return "Table [tableNumber=" + tableNumber + ", seatsCount=" + seatsCount + ", isSmoking=" + isSmoking + "]";
+		return "Table [tableNumber=" + tableNumber + ", seatsCount=" + seatsCount + ", isSmoking=" + isSmoking + ",isReserved="+isReserved+ "]";
+	}
+
+	public boolean isSmoking(boolean isSmoking) {
+		// TODO Auto-generated method stub
+		return false;
 	}
 }
